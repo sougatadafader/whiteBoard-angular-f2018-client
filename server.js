73c90b80e38ee2a4af0cc6c89ced675c5a40903a
sougatadafader <sougatadafader@gmail.com>
2018-11-19 22:11:12 -0500

    .

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/dist/angular-fall-f18')));
+app.use(express.static(path.join(__dirname, '/dist/whiteBoard-angular-f2018-client')));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(express.static(path.join(__dirname,'/dist/angular-fall-f18/index.html')));
+  res.sendFile(express.static(path.join(__dirname,'/dist/whiteBoard-angular-f2018-client/index.html')));
 });
 
 // Start the app by listening on the default Heroku port
